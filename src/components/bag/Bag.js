086ed8c9db9c1b9647ab5fd598b3368f13ccad99
Daniel Bohon <dan@Daniels-MacBook-Pro.local>
2018-12-04 14:21:51 -0700

    stripe front end working with total from cart

--- src/components/bag/Bag.js
+++ src/components/bag/Bag.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import _ from "../utils";
 import "./bag.scss";
 
 export default class Bag extends Component {
   constructor() {
     super();
     this.state = {
-      product: JSON.parse(localStorage.getItem("bagArray")) || []
+      products: JSON.parse(localStorage.getItem("bagArray")) || []
     };
   }
 
@@ -14,16 +16,20 @@ export default class Bag extends Component {
     let itemIndex = tempBag.findIndex(item => item.style === style);
     tempBag.splice(itemIndex, 1);
     localStorage.setItem("bagArray", JSON.stringify(tempBag));
-    this.setState({ product: tempBag });
+    this.setState({ products: tempBag });
   }
 
   render() {
     console.log("---bagArray------->", localStorage.getItem("bagArray"));
-    console.log("this.state.product---------->", this.state.product);
-    const showProducts = this.state.product.map(product => {
+    console.log("this.state.product---------->", this.state.products);
+    let total = _.getTotal(this.state.products);
+
+    console.log("total---------->", total);
+    const showProducts = this.state.products.map(product => {
       return (
         <div>
           <h1>{product.name}</h1>
+          <h3>Price: {product.price}</h3>
           <img src={product.images[0].image} alt="" />
           <button onClick={() => this.removeFromBag(product.style)}>
             Remove from 👜
@@ -31,6 +37,16 @@ export default class Bag extends Component {
         </div>
       );
     });
-    return this.state.product && <div>{showProducts}</div>;
+    return (
+      this.state.products && (
+        <div>
+          <div>{showProducts}</div>
+          <div>Total: {total} </div>
+          <Link to="/checkout">
+            <button>Checkout</button>
+          </Link>
+        </div>
+      )
+    );
   }
 }
