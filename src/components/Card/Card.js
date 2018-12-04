de10f3d90c06d4ca2727538942453e40c056ce90
Christian Ramos <chris12540@gmail.com>
2018-12-04 14:23:19 -0700

    Display component done, product component halfway

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
-import _ from '../utils'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import _ from "../utils";
 
 class Card extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { ...props.item }
+		this.state = { ...props.item };
 	}
 
-	addToBag = (product) => {
-		let localBag = localStorage.getItem('bagArray');
+	addToBag = product => {
+		let localBag = localStorage.getItem("bagArray");
 		if (localBag) {
-			const tempBag = JSON.parse(localStorage.getItem('bagArray'))
-			tempBag.push(product)
-			localStorage.setItem("bagArray", JSON.stringify(tempBag))
+			const tempBag = JSON.parse(localStorage.getItem("bagArray"));
+			tempBag.push(product);
+			localStorage.setItem("bagArray", JSON.stringify(tempBag));
 		} else {
-			localStorage.setItem("bagArray", JSON.stringify([product]))
+			localStorage.setItem("bagArray", JSON.stringify([product]));
 		}
-		console.log('---bagArray------->',localStorage.getItem('bagArray'))
-	}
-
+		console.log("---bagArray------->", localStorage.getItem("bagArray"));
+	};
 
 	render() {
-		let { name, images, price } = this.state
+		let { name, images, price } = this.state;
 		images = _.sortURL(images);
 		return (
 			<div>
 				<h1>{name}</h1>
-				<img src={images[0].image} alt="" />
+				<Link to={window.location.pathname + "/" + name}>
+					<img src={images[0].image} alt="" />
+				</Link>
 				<p>{price}</p>
 				<button onClick={() => this.addToBag(this.state)}>👜</button>
 			</div>
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,19 @@ import Home from "./components/home/Home";
 import Bag from "./components/bag/Bag";
 import Checkout from "./components/checkout/Checkout";
 import Display from "./components/Display/Display";
+// import Product from "./components/product/Product";
 
 export default (
 	<Switch>
 		<Route path="/women/:type" component={Display} />
+		{/* <Route path="/women/:type/name" component={Product} /> */}
 		<Route path="/men/:type" component={Display} />
+		{/* <Route path="/men/:type/name" component={Product} /> */}
 		<Route path="/jewelry-and-watches/:type" component={Display} />
+		{/* <Route path="/jewelry-and-watches/:type/name" component={Product} /> */}
 		<Route path="/beauty/:type" component={Display} />
+		{/* <Route path="/beauty/:type/name" component={Product} /> */}
+
 		<Route path="/checkout" component={Checkout} />
 		<Route path="/bag" component={Bag} />
 		<Route path="/" component={Home} />
