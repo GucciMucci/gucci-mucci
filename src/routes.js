df451f5da6f29b8292fc2f10a06532f34cc8b7fd
Madison Walmsley <madi@Madisons-MacBook-Pro.local>
2018-12-05 16:30:43 -0700

    merges anon and logged in cart, css login page

--- src/routes.js
+++ src/routes.js
@@ -7,20 +7,22 @@ import Checkout from "./components/checkout/Checkout";
 import Display from "./components/Display/Display";
 // import Product from "./components/product/Product";
 import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
 
 export default (
-	<Switch>
-		<Route path="/women/:type" component={Display} />
-		{/* <Route path="/women/:type/name" component={Product} /> */}
-		<Route path="/men/:type" component={Display} />
-		{/* <Route path="/men/:type/name" component={Product} /> */}
-		<Route path="/jewelry-and-watches/:type" component={Display} />
-		{/* <Route path="/jewelry-and-watches/:type/name" component={Product} /> */}
-		<Route path="/beauty/:type" component={Display} />
-		{/* <Route path="/beauty/:type/name" component={Product} /> */}
-		<Route path="/login" component={Login} />
-		<Route path="/checkout" component={Checkout} />
-		<Route path="/bag" component={Bag} />
-		<Route path="/" component={Home} />
-	</Switch>
+  <Switch>
+    <Route path="/women/:type" component={Display} />
+    {/* <Route path="/women/:type/name" component={Product} /> */}
+    <Route path="/men/:type" component={Display} />
+    {/* <Route path="/men/:type/name" component={Product} /> */}
+    <Route path="/jewelry-and-watches/:type" component={Display} />
+    {/* <Route path="/jewelry-and-watches/:type/name" component={Product} /> */}
+    <Route path="/beauty/:type" component={Display} />
+    {/* <Route path="/beauty/:type/name" component={Product} /> */}
+    <Route path="/login" component={Login} />
+    <Route path="/checkout" component={Checkout} />
+    <Route path="/bag" component={Bag} />
+    <Route path="/profile" component={Profile} />
+    <Route path="/" component={Home} />
+  </Switch>
 );
