1e3077dd62b2f98614211f992b08d7d87b3d5636
Madison Walmsley <madi@Madisons-MacBook-Pro.local>
2018-12-10 17:29:38 -0700

    started homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,32 +4,38 @@ import Card from "../Card/Card";
 import firebase from "../firebase";
 
 export default class Home extends Component {
-	constructor() {
-		super();
-		this.state = {
-			username: "",
-			currentItem: "",
-			data: []
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      currentItem: "",
+      data: []
+    };
+  }
 
-	componentDidMount() {
-		firebase
-			.database()
-			.ref("frontpage")
-			.once("value")
-			.then(snapshot => {
-				this.setState({
-					data: snapshot.val()
-				});
-			});
-	}
+  componentDidMount() {
+    firebase
+      .database()
+      .ref("frontpage")
+      .once("value")
+      .then(snapshot => {
+        this.setState({
+          data: snapshot.val()
+        });
+      });
+  }
 
-	render() {
-		return (
-			<div>
-				<p>Stuff goes here</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div className="home-header">
+          <div className="head-content">
+            <div>GUCCI IN COLLABORATION WITH</div>
+            <h1>CHATEAU MARMONT</h1>
+            <button>SHOP</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
